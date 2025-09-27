4c7c635dc75805be72d3fe87a4c4820bdfb836e5
ArseniyFursov <akseniy.life@gmail.com>
2025-09-27 22:23:11 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let posts = [];
+const inputTitlePost = document.querySelector('.js-input-title-post');
+const inputTextPost = document.querySelector('.js-input-text-post');
+
+function addEventPost() {
+
+}
+
+function addPost() {
+    let post = {
+        title: inputTitlePost.value,
+        text: inputTextPost.value,
+    }
+
+
+}
